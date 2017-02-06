9d925e1f3ff9ea4253f29a18508cb3dfcfd75c50
Marius <mlatinis@gmail.com>
2017-02-06 07:41:54 +0000

    Improved readability of RoutesExtractor for NAPTAN data

--- RoutesExtractor.java
+++ RoutesExtractor.java
@@ -1,4 +1,8 @@
 /*
+ * A program which is meant to be executed only once. It builds convenient route
+ * representation (as a list of bus stops) from the open source input files
+ * given.
+ * 
  * Program takes stop_times.txt, stops.txt files and extracts all possible
  * routes, each route being saved in a separate file.
  * 
@@ -20,7 +24,7 @@ public class RoutesExtractor {
 
 	static HashMap<Long, BusStop> busStops = new HashMap<Long, BusStop>();
 
-	static HashMap<String, BusStop> busStopsNew = new HashMap<String, BusStop>();
+	static HashMap<String, BusStop> busStopsFromNaptan = new HashMap<String, BusStop>();
 
 	static Set<String> duplicateRoute = new HashSet<String>();
 
@@ -43,47 +47,6 @@ public class RoutesExtractor {
 			busStops.put(stopId, busStop);
 		}
 		scanner.close();
-	}
-
-	static void readBusStopsNew(File stopsFile) throws IOException {
-		BufferedReader stopsInput = new BufferedReader(
-				new FileReader(stopsFile));
-		String line = stopsInput.readLine();
-
-		while (line != null) {
-			while (!line.contains("<AtcoCode>")) {
-				line = stopsInput.readLine();
-			}
-			String code = line.substring(6 + 10, line.length() - 11);
-
-			while (!line.contains("<CommonName>")) {
-				line = stopsInput.readLine();
-			}
-			String name = line.substring(8 + 12, line.length() - 13);
-
-			while (!line.contains("<Longitude>")) {
-				line = stopsInput.readLine();
-			}
-			double longitude = Double
-					.parseDouble(line.substring(12 + 11, line.length() - 12));
-
-			while (!line.contains("<Latitude>")) {
-				line = stopsInput.readLine();
-			}
-			double latitude = Double
-					.parseDouble(line.substring(12 + 10, line.length() - 11));
-
-			busStopsNew.put(code, new BusStop(name, latitude, longitude));
-
-			while (!line.contains("</StopPoint>")) {
-				line = stopsInput.readLine();
-			}
-			if (!stopsInput.readLine().contains("<StopPoint")) {
-				break;
-			}
-		}
-
-		stopsInput.close();
 	}
 
 	/* WARNING: tripsFile has to be well formatted! */
@@ -124,59 +87,127 @@ public class RoutesExtractor {
 		}
 	}
 
-	static void constructRoutesNew(File tripFile, File routesFolder)
-			throws IOException {
+	static void readBusStopsFromNaptanFiles(File stopsFile) throws IOException {
 		BufferedReader stopsInput = new BufferedReader(
-				new FileReader(tripFile));
+				new FileReader(stopsFile));
 		String line = stopsInput.readLine();
 
-		String fileName = tripFile.getName();
-		Route route = new Route(fileName.substring(0, fileName.length() - 4),
-				new ArrayList<BusStop>());
 		while (line != null) {
-			while (!line.contains("<StopPointRef>")) {
+			/*
+			 * TODO(ml693): I forgot what 6, 10, 12, etc means. I suspect these
+			 * spacing numbers. Need to figure that out.
+			 */
+			while (!line.contains("<AtcoCode>")) {
 				line = stopsInput.readLine();
 			}
-			String code = line.substring(6 + 14, line.length() - 15);
-			route.busStops.add(busStopsNew.get(code));
+			String code = line.substring(6 + 10, line.length() - 11);
 
-			while (!line.contains("</AnnotatedStopPointRef>")) {
+			while (!line.contains("<CommonName>")) {
 				line = stopsInput.readLine();
 			}
-			if (!stopsInput.readLine().contains("<AnnotatedStopPointRef>")) {
+			String name = line.substring(8 + 12, line.length() - 13);
+
+			while (!line.contains("<Longitude>")) {
+				line = stopsInput.readLine();
+			}
+			double longitude = Double
+					.parseDouble(line.substring(12 + 11, line.length() - 12));
+
+			while (!line.contains("<Latitude>")) {
+				line = stopsInput.readLine();
+			}
+			double latitude = Double
+					.parseDouble(line.substring(12 + 10, line.length() - 11));
+
+			busStopsFromNaptan.put(code,
+					new BusStop(name, latitude, longitude));
+
+			while (!line.contains("</StopPoint>")) {
+				line = stopsInput.readLine();
+			}
+			if (!stopsInput.readLine().contains("<StopPoint")) {
 				break;
 			}
 		}
+
 		stopsInput.close();
+	}
 
-		System.out.println("Route " + route.name + " consists of "
-				+ route.busStops.size() + " stops.");
+	static void extractRoutesFromNaptanFiles(File tripFile,
+			File folderToSaveRoute) {
+		final String stopPointRef = "<StopPointRef>";
+		final String annotatedRefOpen = "<AnnotatedStopPointRef>";
+		final String annotatedRefClose = "</AnnotatedStopPointRef>";
 
-		String serializedRoute = route.serialize();
-		if (!duplicateRoute.contains(serializedRoute)) {
-			duplicateRoute.add(serializedRoute);
-			route.writeToFolder(routesFolder);
-		} else {
-			System.out.println("Duplicate " + route.name + " found!");
+		try {
+			BufferedReader stopsInput = new BufferedReader(
+					new FileReader(tripFile));
+			String line = stopsInput.readLine();
+			String fileName = tripFile.getName();
+			Route route = new Route(
+					fileName.substring(0,
+							fileName.length() - 4 /* to remove .xml part */),
+					new ArrayList<BusStop>());
+
+			while (line != null) {
+				while (!line.contains(stopPointRef)) {
+					line = stopsInput.readLine();
+				}
+				/* 6 and 15 is the lenght of spaces in that line */
+				String code = line.substring(6 + stopPointRef.length(),
+						line.length() - 15);
+				route.busStops.add(busStopsFromNaptan.get(code));
+
+				while (!line.contains(annotatedRefClose)) {
+					line = stopsInput.readLine();
+				}
+				if (!stopsInput.readLine().contains(annotatedRefOpen)) {
+					break;
+				}
+			}
+			stopsInput.close();
+
+			System.out.println("Route " + route.name + " consists of "
+					+ route.busStops.size() + " stops.");
+
+			String serializedRoute = route.serialize();
+			if (!duplicateRoute.contains(serializedRoute)) {
+				duplicateRoute.add(serializedRoute);
+				route.writeToFolder(folderToSaveRoute);
+			} else {
+				System.out.println("Duplicate " + route.name + " found!");
+			}
+
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
 		}
+	}
 
+	static void constructRoutesFromNaptanData(File stopsFile, File routesFolder,
+			File outputRoutesFolder) {
+		try {
+			readBusStopsFromNaptanFiles(stopsFile);
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
+		}
+		System.out
+				.println("Extracted " + busStopsFromNaptan.size() + " stops.");
+
+		File[] routesFiles = routesFolder.listFiles();
+		for (File routeFile : routesFiles) {
+			extractRoutesFromNaptanFiles(routeFile, outputRoutesFolder);
+		}
 	}
 
 	public static void main(String args[]) throws Exception {
 		/*
-		 * 1st argument is a folder containing stops.txt, stop_times.txt files.
-		 * 2nd argument is a folder where to put routes.
+		 * 1st argument is a file of stop locations.
+		 * 2nd argument is a folder containing file per route.
+		 * 3rd argument is a folder where to output routes.
 		 */
 		Utils.checkCommandLineArguments(args, "file", "folder", "folder");
-
-		readBusStopsNew(new File(args[0]));
-		System.out.println("Extracted " + busStopsNew.size() + " stops.");
-
-		File[] routesFiles = new File(args[1]).listFiles();
-		File outputRoutesFolder = new File(args[2]);
-		for (File routeFile : routesFiles) {
-			constructRoutesNew(routeFile, outputRoutesFolder);
-		}
+		constructRoutesFromNaptanData(new File(args[0]), new File(args[1]),
+				new File(args[2]));
 	}
 
 }
