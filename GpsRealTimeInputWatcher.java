9b443950408e96eba03c1c3a5e88983de99c775a
Marius <mlatinis@gmail.com>
2017-04-10 20:21:57 +0300

    Adding trip.name for prediction.

--- GpsRealTimeInputWatcher.java
+++ GpsRealTimeInputWatcher.java
@@ -318,6 +318,7 @@ class GpsRealTimeInputWatcher {
 
 				Prediction prediction = ArrivalTimePredictor.makePrediction(
 						route.lastStop(), trip, historicalTrips);
+				prediction.name = trip.name;
 				prediction.predictionTimestamp = trip.lastPoint().timestamp;
 				prediction.route = route;
 				prediction.fromStopIndex = recentStopIndex;
